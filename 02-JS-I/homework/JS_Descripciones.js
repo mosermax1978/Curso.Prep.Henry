bf06814bfca1b4af917ddd7a6220abe6658b6a05
mosermax1978 <maximiliano.a.moser@gmail.com>
2021-11-11 17:28:41 -0300

    Archivo con definiciones homework, JS_Descripciones.js

---
+++ 02-JS-I/homework/JS_Descripciones.js
@@ -0,0 +1,18 @@
+Conceptos
+
+Variables: Una variable es algo que definimos para luego poder utilizarlo. Por ejemplo podemos crear
+con el comando "var" una variable "X" y asignarle un valor. Puede ser texto o numerico. Tambien se
+puede crear una variable con el comando "const", la cual se fija un valor que no se puede cambiar.
+
+Strings: Son textos, palablas o cadenas de palabras. Los strings van siempre entre comillas.
+
+Funiones (Argumentos, Return): Las funciones sirven para modificar o calcular una o mas variables. Para ello 
+utilizamos la palabra "function" a la cual debemos dar un nombre, para luego terminar con perentesis (). 
+Y luego entre llaves ira lo que queremos que la funcion haga. Podemos agregar una variable entr los parentesis
+para utilizarla como tal. 
+Debemos utilizar "Return" para lograr que la funcion nos devuelva lo que queramos obtener de la funcion.
+
+Declaraciones if: If es un control de flujo que se utiliza para que la funcion nos devuelva un true o false.
+
+Valores booleanos (true, false): Los valores booleanos solo tienen como respuesta true o false.
+Esto quiere decir que podemos definir una variable como algo verdadero o falso.
